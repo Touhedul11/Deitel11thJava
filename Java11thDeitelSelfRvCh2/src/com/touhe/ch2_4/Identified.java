53245d5cf959ba76956eab3aa42cf2de14ac5673
Touhedul Islam <49379202+Touhedul11@users.noreply.github.com>
2022-07-06 23:36:09 -0400

    Add files via upload
    This is the solution of Chapter 2

---
+++ Java11thDeitelSelfRvCh2/src/com/touhe/ch2_4/Identified.java
@@ -0,0 +1,20 @@
+package com.touhe.ch2_4;
+
+public class Identified {
+
+	public static void main(String[] args) {
+		/*Identify and correct the errors in each of the following statements:
+		1. if (c < 7); { System.out.println("c is less
+				than 7"); }
+				2. if (c => 7) { System.out.println("c is equal
+				to or greater than 7"); }
+				*/
+		int c = 0;
+		if (c < 7) { System.out.println("c is less than 7");
+			}
+		
+		if (c >=7) { System.out.println("c is equal to or greater than 7"); }
+
+	}
+
+}
